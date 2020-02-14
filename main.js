d645dd34ea7eb0bacc8d81a4e3d558ccd0f101a0
Josiah Moore <44247369+JSMoore98@users.noreply.github.com>
2020-02-14 16:24:14 -0500

    Cleaned up the testing console logs in Display

--- main.js
+++ main.js
@@ -72,7 +72,6 @@ function executeCode(line) {
           for (var i=0; i < print1.length; i++) {
             //Checks for spaces
             if (print1.charAt(i) == " ") {
-                console.log("space")
                 //Prints the space if we're in the middle of a string
                 if (isString)
                     document.getElementById('console').innerHTML += print1.charAt(i);
@@ -80,7 +79,6 @@ function executeCode(line) {
             }
             //Checks for commas
             else if (print1.charAt(i) == ",") {
-                console.log("comma")
                 //Prints the comma if we're in the middle of a string
                 if (isString)
                     document.getElementById('console').innerHTML += print1.charAt(i);
@@ -88,7 +86,6 @@ function executeCode(line) {
             }
             //Checks for quotes
             else if (print1.charAt(i) == "\"") {
-                console.log("quote")
                 if (isString == false)
                     isString = true;
                 else
@@ -96,32 +93,21 @@ function executeCode(line) {
             }
             //Checks for anything else, meaning a variable name has started
             else {
-                console.log("nothing")
                 //If it's in a quoted string, print it
                 if (isString)
                     document.getElementById('console').innerHTML += print1.charAt(i);
                 else {
                     //Find the index where the variable ends
                     var spaceIndex = print1.substring(i,print1.length).indexOf(",")
-                    console.log(print1.substring(i,print1.length))
-                    console.log("Space" + spaceIndex)
                     //Checks if the line ends after this variable
                     if (spaceIndex <= 0)
                         spaceIndex = print1.length - i;
-                    console.log("Space" + spaceIndex)
 
                     //Grabs the variable name from current point to the comma/end of line
                     var VariableName = print1.substring(i,(i + spaceIndex))
-                    console.log(VariableName)
-                    console.log(VariableName.length)
                     //Move i forward to the end of the variable name
                     i = i + VariableName.length
-                    console.log("V: "+ VariableName)
-                    console.log(VariableName.length)
                     VariableName = VariableName.trim()
-                    console.log("V: "+ VariableName)
-                    console.log(VariableName.length)
-                    console.log(i)
                     if (checkVariableExistance(VariableName)) {
                         //Getting the variable needs to be updated
                         document.getElementById('console').innerHTML += getVariable(VariableName);
